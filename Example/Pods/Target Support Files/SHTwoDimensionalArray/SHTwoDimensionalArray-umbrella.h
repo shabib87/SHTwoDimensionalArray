f3eb11019465b4e667076799996f5a7ac8dc4f0c
Ahmad Shabibul Hossain <shabib.hossain@iappdragon.com>
2016-08-17 13:33:57 +0600

    Source library class and documentation.

--- Example/Pods/Target Support Files/SHTwoDimensionalArray/SHTwoDimensionalArray-umbrella.h
+++ Example/Pods/Target Support Files/SHTwoDimensionalArray/SHTwoDimensionalArray-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "SHTwoDimensionalArray.h"
 
 FOUNDATION_EXPORT double SHTwoDimensionalArrayVersionNumber;
 FOUNDATION_EXPORT const unsigned char SHTwoDimensionalArrayVersionString[];
--- SHTwoDimensionalArray/Classes/SHTwoDimensionalArray.h
+++ SHTwoDimensionalArray/Classes/SHTwoDimensionalArray.h
@@ -10,9 +10,39 @@
 
 @interface SHTwoDimensionalArray : NSObject
 
+/**
+ *  @author Shabib Hossain
+ *
+ *  Constructor method for the two dimensional array.
+ *
+ *  @param rows Number of rows to be in the 2D array.
+ *  @param columns Number of columns to be in the 2D array.
+ *
+ *  @return An initialized empty 2D array witht he desired number of rows and columns.
+ */
 + (id)arrayWithRows:(NSUInteger)rows andColumns:(NSUInteger)columns;
 
+/**
+ *  @author Shabib Hossain
+ *
+ *  Returns the object at desired row and column position.
+ *
+ *  @param row    Desired object's row position.
+ *  @param column Desired object's column position.
+ *
+ *  @return Object at the [row][column] position.
+ */
 - (id)objectInRow:(NSUInteger)row column:(NSUInteger)column;
+
+/**
+ *  @author Shabib Hossain
+ *
+ *  Sets an object at the desired [row][column] position.
+ *
+ *  @param obj    The object that is to be placed in the array.
+ *  @param row    Index of the row.
+ *  @param column Index of the column.
+ */
 - (void)setObject:(id)obj inRow:(NSUInteger)row column:(NSUInteger)column;
 
 @end
